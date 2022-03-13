74ab7021b426bbed1b6ba67575463f98ef4fcf34
Parth Sarin <psarin@stanford.edu>
2022-03-13 12:32:53 -0700

    Initial commit

---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app flex justify-center items-center h-full w-full">
+      <h1 className="text-3xl text-white">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
